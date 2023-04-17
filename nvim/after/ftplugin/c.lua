e1c6448208696f326ce629966775953189064335
Niko Pavlinek <niko.pavlinek@gmail.com>
2023-04-17 17:00:26 +0200

    nvim: Cleanup

--- nvim/after/ftplugin/c.lua
+++ nvim/after/ftplugin/c.lua
@@ -1,7 +1,6 @@
 vim.opt_local.cindent = true
 vim.opt_local.cinoptions = {
     "g0", -- No C++ scope declaration indentation.
-    "l1", -- Indent case label contents as a scope.
 }
 
 vim.opt.formatoptions:remove("c")
