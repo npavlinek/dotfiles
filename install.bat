1bc794c3a8b2050be01c4a4c5363b129a482fe10
Niko Pavlinek <niko.pavlinek@gmail.com>
2023-07-10 17:42:37 +0200

    Quote paths in install scripts

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 @echo off
 
-mklink      %USERPROFILE%\.gitconfig        %CD%\gitconfig
-mklink      %USERPROFILE%\mercurial.ini     %CD%\hgrc
-mklink /D   %APPDATA%\.emacs.d              %CD%\emacs
-mklink /D   %USERPROFILE%\vimfiles          %CD%\vim
+mklink "%USERPROFILE%\.gitconfig" "%CD%\gitconfig"
+mklink "%USERPROFILE%\mercurial.ini" "%CD%\hgrc"
+mklink /D "%APPDATA%\.emacs.d" "%CD%\emacs"
+mklink /D "%USERPROFILE%\vimfiles" "%CD%\vim"
