7535a654fe1910cbbf8064a552c6ded2736d7460
Niko Pavlinek <niko.pavlinek@gmail.com>
2023-07-11 12:51:59 +0200

    Remove Emacs configuration

--- install.bat
+++ install.bat
@@ -2,5 +2,4 @@
 
 mklink "%USERPROFILE%\.gitconfig" "%CD%\gitconfig"
 mklink "%USERPROFILE%\mercurial.ini" "%CD%\hgrc"
-mklink /D "%APPDATA%\.emacs.d" "%CD%\emacs"
 mklink /D "%USERPROFILE%\vimfiles" "%CD%\vim"
