a86b2c8f6f90e319143e36c5fd38d40a321d3314
Niko Pavlinek <niko.pavlinek@gmail.com>
2023-07-11 16:37:45 +0200

    Don't dereference symbolic links

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-ln -s "$PWD/gitconfig" "$HOME/.gitconfig"
-ln -s "$PWD/hgrc" "$HOME/.config/hg/hgrc"
-ln -s "$PWD/openbox" "$HOME/.config/openbox"
-ln -s "$PWD/vim" "$HOME/.vim"
-ln -s "$PWD/xinitrc" "$HOME/.xinitrc"
+ln -n -s "$PWD/gitconfig" "$HOME/.gitconfig"
+ln -n -s "$PWD/hgrc" "$HOME/.config/hg/hgrc"
+ln -n -s "$PWD/openbox" "$HOME/.config/openbox"
+ln -n -s "$PWD/vim" "$HOME/.vim"
+ln -n -s "$PWD/xinitrc" "$HOME/.xinitrc"
