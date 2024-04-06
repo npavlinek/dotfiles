f01eeaae978ac2e8c15566991495b8819d6caf63
Niko Pavlinek <niko.pavlinek@gmail.com>
2024-04-06 09:05:52 +0200

    cleanup

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-ln -nrs "$PWD/gitconfig" "$HOME/.gitconfig"
-ln -nrs "$PWD/hgrc" "$HOME/.hgrc"
-ln -nrs "$PWD/i3" "$HOME/.config/i3"
-ln -nrs "$PWD/vim" "$HOME/.vim"
-ln -nrs "$PWD/xinitrc" "$HOME/.xinitrc"
+ln -ns "$PWD/gitconfig" "$HOME/.gitconfig"
+ln -ns "$PWD/hgrc" "$HOME/.hgrc"
+ln -ns "$PWD/i3" "$HOME/.config/i3"
+ln -ns "$PWD/vim" "$HOME/.vim"
+ln -ns "$PWD/xinitrc" "$HOME/.xinitrc"
