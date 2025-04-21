ce5140222231a1adf9790593a583ce0c32bde277
Niko Pavlinek <niko.pavlinek@gmail.com>
2025-04-21 13:57:34 +0200

    Remove i3 config

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 if [ "$(uname)" = Linux ] || [ -n "$MSYSCON" ]; then
     ln -ns "$PWD/gitconfig" "$HOME/.gitconfig"
     ln -ns "$PWD/hgrc" "$HOME/.hgrc"
-    ln -ns "$PWD/i3" "$HOME/.config/i3"
     ln -ns "$PWD/vim" "$HOME/.vim"
     ln -ns "$PWD/xinitrc" "$HOME/.xinitrc"
 elif [ "$OS" = Windows_NT ]; then
