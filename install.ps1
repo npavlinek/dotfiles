64d0610821879400633456cadc684483770388c2
Niko Pavlinek <niko.pavlinek@gmail.com>
2025-11-02 16:37:34 +0100

    Add alacritty config

--- install.ps1
+++ install.ps1
@@ -1,2 +1,3 @@
+cmd /c "mklink /d `"%APPDATA%\alacritty`" `"%CD%\alacritty`""
 cmd /c "mklink /d `"%USERPROFILE%\vimfiles`" `"%CD%\vim`""
 cmd /c "mklink `"%USERPROFILE%\.gitconfig`" `"%CD%\gitconfig`""
--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+ln -ns "$PWD/alacritty" "$HOME/.config/alacritty"
 ln -ns "$PWD/gitconfig" "$HOME/.gitconfig"
 ln -ns "$PWD/vim" "$HOME/.vim"
